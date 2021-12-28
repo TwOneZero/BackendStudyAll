14c248ec93b4944b092fe933dba59a6e2e35b8d2
twoonezero <dnjsdudtm98@naver.com>
2021-12-28 23:17:46 +0900

    API connected

--- myserver/src/index.js
+++ myserver/src/index.js
@@ -1,8 +1,14 @@
 const path = require("path"); // npm core library 이어서 npm으로 설치할 필요 없음
 const express = require("express"); // import express from 'express'=> es6 문법은 nodejs 에서 안 쓰임
-const hbs = require("hbs");
-
+const hbs = require("hbs"); //view template engine
+const bodyParser = require("body-parser");
+const airdata = require("./utils/airdata");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Heroku 에 올릴거라서 port 번호를 변수화
+const port = process.env.PORT || 5000;
 
 const publicDirPath = path.join(__dirname, "../public"); // app 에 static 파일을 넘겨주기 위함
 //template 경로
@@ -14,52 +20,43 @@ app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 app.use(express.static(publicDirPath)); //인자로 들어온 걸 express 에 던져줌
 
-// console.log(__dirname); // node 에 내장된 디렉토리 절대경로 변수
-// console.log(__filename); //파일 까지
-
-//route handler
-// app.get("/", (req, res) => {
-//   res.send("안녕하세요");
-// });
-// app.get("/help", (req, res) => {
-//   res.send("도움이 필요하십니까?");
-// });
-// app.get("/about", (req, res) => {
-//   res.send("About...");
-// });
-
 app.get("/", (req, res) => {
   res.render("index", {
     title: "미세먼지 정보 앱",
-    name: "이원영",
-    email: "123123google.com",
   });
 });
 
 app.get("/help", (req, res) => {
   res.render("help", {
     title: "무엇을 도와드릴까요?",
-    name: "삼원영",
-    email: "google.com",
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "앱 정보",
-    name: "사원영",
-    email: "@@@@google.com",
     message: "About Page,,,hbs 연습중ㅎㅎ",
   });
 });
 
-app.get("/air", (req, res) => {
-  res.send({
-    forecast: "현재는 쌀쌀",
-    location: "서울",
+app.post("/air", (req, res) => {
+  airdata(req.body.location, (error, { air } = {}) => {
+    if (error) {
+      return res.send({ error });
+    }
+    console.log("========");
+    const airItem = air.response.body.items;
+    console.log(air.response.body.items);
+    return res.render("air", {
+      title: "미세먼지 정보",
+      location: req.body.location,
+      time: airItem[0].dataTime,
+      pm10: airItem[0].pm10Value,
+      pm25: airItem[0].pm25Value,
+    });
   });
 });
 
-app.listen(5000, () => {
-  console.log("connected!");
-}); // port number - > 5000
+app.listen(port, () => {
+  console.log(`connected! Running at ${port}`);
+});
